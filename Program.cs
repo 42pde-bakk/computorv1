3b1743576e451f9c584877bc146e46dbddf61d8a
peerdebakker <peer.de.bakker@panartis.com>
2022-05-09 23:40:28 +0200

    workable

--- Program.cs
+++ Program.cs
@@ -2,47 +2,81 @@
 
 using System.Text.RegularExpressions;
 
-static Dictionary<double, double> parseSide(string arg)
+static Double Sqrt(Double nb)
+{
+	if (nb <= 0)
+	{
+		return (double.NaN);
+	}
+	Double root = nb / 3;
+	Int32 i;
+	for (i = 0; i < 32; i++)
+		root = (root + nb / root) / 2;
+	return (root);
+}
+
+static Dictionary<Int32, Double> ParseSide(String arg)
 {
 	Console.WriteLine($"lets parse {arg}");
 	Regex r = new Regex(@"([+-]?[\d+]?.?[\d+]?)\*X\^([+-]?[\d+])", RegexOptions.IgnoreCase);
-	Dictionary<double, double> dict = new Dictionary<double, double>
+	Dictionary<Int32, Double> dict = new()
 	{
 		{0, 0},
 		{1, 0},
 		{2, 0}
 	};
-	string[] parts = Regex.Split(arg, @"(?=[+-])");
+	String[] parts = Regex.Split(arg, @"(?=[+-])");
 
-	foreach (string part in parts)
+	foreach (String part in parts)
 	{
 		Console.WriteLine($"part = {part}");
 		Match match = r.Match(part);
 		if (match.Success)
 		{
-			foreach (object? group in match.Groups)
+			foreach (Object? group in match.Groups)
 			{
 				Console.WriteLine($"group: {group}");
 			}
 			// parse it like a * x^p
-			double coeff = double.Parse(match.Groups[1].Value);
-			double power = double.Parse(match.Groups[2].Value);
+			Double coeff = double.Parse(match.Groups[1].Value);
+			Int32 power = int.Parse(match.Groups[2].Value);
 			dict[power] += coeff;
 		}
 		else
 		{
 			// maybe its just a regular int/float value
+			Int32 coeff = int.Parse(part);
+			dict[0] += coeff;
 		}
 	}
 	return dict;
 }
 
-static Dictionary<double, double> parse(string arg)
+void Solve(IReadOnlyDictionary<Int32, Double> d)
 {
-	string arg2 = Regex.Replace(arg, @"\s+", "");
+	Double  a = d[2],
+			b = d[1],
+			c = d[0];
+	Double discriminant = (b * b) - (4 * a * c);
+	Double sol1 = (-b - Sqrt(discriminant)) / (2 * a);
+	Double sol2 = (+b - Sqrt(discriminant)) / (2 * a);
+	Console.WriteLine($"The two solutions are:\n{sol1}\n{sol2}");
+}
+
+static Dictionary<Int32, Double> parse(String arg)
+{
+	String arg2 = Regex.Replace(arg, @"\s+", "");
 	Console.WriteLine($"arg={arg}, arg2={arg2}");
-	var splitted = arg2.Split('=');
-	return parseSide(splitted[0]);
+	String[] splitted = arg2.Split('=');
+	Dictionary<Int32, Double> lhs = ParseSide(splitted[0]);
+	Dictionary<Int32, Double> rhs = ParseSide(splitted[1]);
+	
+	return new Dictionary<Int32, Double>
+	{
+		{0, lhs[0] - rhs[0]},
+		{1, lhs[1] - rhs[1]},
+		{2, lhs[2] - rhs[2]}
+	};
 }
 
 Console.WriteLine("Hello, World!");
@@ -52,4 +86,6 @@ if (args.Length != 1)
 	Console.WriteLine("Error. Please provide your equation within quotes");
 	Environment.Exit(1);
 }
-var dict = parse(args[0]);
+Dictionary<Int32, Double> dict = parse(args[0]);
+Console.WriteLine($"0: {dict[0]}, 1: {dict[1]}, 2: {dict[2]}");
+Solve(dict);
