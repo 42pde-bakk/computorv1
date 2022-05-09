59cad28cadc5456baf3728bdb93f52e00270292a
peerdebakker <peer.de.bakker@panartis.com>
2022-05-09 03:18:27 +0200

    parsing done for real things, not yet for empties

--- Program.cs
+++ Program.cs
@@ -12,47 +12,28 @@ static Dictionary<double, double> parseSide(string arg)
 		{1, 0},
 		{2, 0}
 	};
-	string[] parts = Regex.Split(arg, @"(?<=[+-])");
+	string[] parts = Regex.Split(arg, @"(?=[+-])");
 
 	foreach (string part in parts)
 	{
+		Console.WriteLine($"part = {part}");
 		Match match = r.Match(part);
 		if (match.Success)
 		{
-			foreach (var group in match.Groups)
+			foreach (object? group in match.Groups)
 			{
 				Console.WriteLine($"group: {group}");
 			}
 			// parse it like a * x^p
-			double a = double.Parse(match.Groups[1].Value);
+			double coeff = double.Parse(match.Groups[1].Value);
 			double power = double.Parse(match.Groups[2].Value);
-			dict[power] += a;
+			dict[power] += coeff;
 		}
 		else
 		{
 			// maybe its just a regular int/float value
 		}
 	}
-	
-	// // Regex r = new Regex(@"\d+X^\d+", RegexOptions.IgnoreCase);
-	// // Regex r = new Regex(@"(?<coe>[-+]?[0-9]*\.[0-9]+)", RegexOptions.IgnoreCase);
-	// Match match = r.Match(arg);
-	// Console.WriteLine($"match.Success: {match.Success}, groups: {match.Groups.Count}");
-	// while (match.Success)
-	// {
-	// 	for (Int32 i = 1; i < 2; i++)
-	// 	{
-	// 		Group g = match.Groups[i];
-	// 		Console.WriteLine("Group"+i+"='" + g + "'");
-	// 		CaptureCollection cc = g.Captures;
-	// 		for (int j = 0; j < cc.Count; j++)
-	// 		{
-	// 			Capture c = cc[j];
-	// 			Console.WriteLine("Capture"+j+"='" + c + "', Position="+c.Index);
-	// 		}
-	// 	}
-	// 	match = match.NextMatch();
-	// }
 	return dict;
 }
 
