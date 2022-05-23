ff0414ff52fa0d2ae7858d0c75d7110564e7607a
pde-bakk <pde-bakk@student.codam.nl>
2022-05-23 15:42:05 +0200

    added bonus export

--- test.sh
+++ test.sh
@@ -10,6 +10,9 @@ function run_testcase() {
 	echo
 }
 
+## For Bonus things:
+# export COMPUTORV1_BONUS=1
+
 run_testcase "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0" # -9.3X^2 + 4X + 4 = 0
 run_testcase "5 * X^0 + 4 * X^1 = 4 * X^0" # 4X = -1
 run_testcase "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0" # -5.6X^3 - 6X + 5 = 0
