86976a81a16e2f96c062510823c438d229d7c45b
pde-bakk <pde-bakk@student.codam.nl>
2022-07-12 13:26:37 +0200

    gave my test script an option to wait after each testcase, for easier verification that it is correct

--- test.sh
+++ test.sh
@@ -6,6 +6,18 @@ END=$'\e[0m'
 
 EXE="bin/Debug/net6.0/computorv1"
 
+SHOULD_WAIT=$1
+
+function wait_for_keypress() {
+    echo "Press any key to continue"
+    while true ; do
+      read -t 3 -n 1
+    if [ $? = 0 ] ; then
+      return ;
+    fi
+    done
+}
+
 function run_testcase() {
 	EXPECTED_EXIT_STATUS=$1
 	EQUATION=$2
@@ -17,10 +29,13 @@ function run_testcase() {
 	  exit 1
 	fi
 	echo "[${GREEN}OK${END}] in ${EQUATION}"; echo
+	if [[ ${SHOULD_WAIT} == "wait" ]]; then
+	  wait_for_keypress
+  fi
 }
 
 ## For Bonus things:
-# export COMPUTORV1_BONUS=1
+#export COMPUTORV1_BONUS=1
 
 run_testcase 0 "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0" # -9.3X^2 + 4X + 4 = 0
 run_testcase 0 "5 * X^0 + 4 * X^1 = 4 * X^0" # 4X = -1
